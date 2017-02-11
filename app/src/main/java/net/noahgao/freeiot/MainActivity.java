f5c09863fd45424f13e37b75c462dda79da701c0
Noah Gao <noahgaocn@gmail.com>
2017-02-11 23:31:52 +0800

    0211 NIGHTLY

--- app/src/main/java/net/noahgao/freeiot/MainActivity.java
+++ app/src/main/java/net/noahgao/freeiot/MainActivity.java
@@ -77,28 +77,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -110,8 +88,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_tools) {
 
         } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_about) {
 
         } else if (id == R.id.nav_logout) {
             Auth.logout();
