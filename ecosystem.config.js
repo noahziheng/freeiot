c2ad0bbceb314cade71faa75d694615c3bc45235
Noah Gao <noahgaocn@outlook.com>
2017-12-07 20:47:25 +0800

    Add Deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'FreeIOT-API',
+      script    : 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production',
+        PORT: 2335
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '121.42.231.226',
+      ref  : 'origin/master',
+      repo : 'git@github.com:noahziheng/freeiot-api.git',
+      path : '/data/www/freeiot-api',
+      'post-deploy' : 'cp config.js.sample config.js && yarn install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '121.42.231.226',
+      ref  : 'origin/master',
+      repo : 'git@github.com:noahziheng/freeiot-api.git',
+      path : '/data/www/freeiot-api',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
