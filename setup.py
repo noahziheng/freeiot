6bc18e48b78667af19ed71cbceab307042a9f791
Noah Gao <noahgaocn@outlook.com>
2018-03-25 12:58:05 +0800

    Version update

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "libfreeiot",
-    version = "0.9.3",
+    version = "0.9.4",
     description = 'A free, open-source IoT Framework',
     author = 'Noah Gao',
     author_email = 'noahgaocn@outlook.com',
     url = 'https://github.com/noahziheng/freeiot',
-    download_url = 'https://github.com/noahziheng/freeiot/archive/0.9.3.tar.gz',
+    download_url = 'https://github.com/noahziheng/freeiot/archive/0.9.4.tar.gz',
     packages = find_packages(),
     install_requires=[    # 依赖列表
         'Flask>=0.12.2',
