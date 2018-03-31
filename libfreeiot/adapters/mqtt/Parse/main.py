361407a75c3c0388c694a9e20e2f073cc930d28a
Noah Gao <noahgaocn@outlook.com>
2018-03-31 18:50:07 +0800

    0.9.6 & Fix mqtt invalid deviceId cause error

--- libfreeiot/adapters/mqtt/Parse/main.py
+++ libfreeiot/adapters/mqtt/Parse/main.py
@@ -9,6 +9,11 @@ from . import sys, mongo
 
 def before_check(d_id):
     """ Before Check Function """
+    if not ObjectId.is_valid(d_id):
+        print(d_id + " is invalid!")
+        return
+    else:
+        d_id = ObjectId(d_id)
     result = mongo.db.devices.find_one({"_id": d_id})
     if result is not None:
         if "status" not in dict(result).keys():
@@ -21,14 +26,14 @@ def main(client, topic, payload):
     """ Parse Engine Main Function """
     print("Parsing ", topic, payload)
     if topic[0]=='SYS':
-        if not before_check(ObjectId(payload["id"])):
+        if not before_check(payload["id"]):
             return
         if topic[1]=='online':
             sys.online(client, payload)
         elif topic[1]=='will':
             sys.offline(client, payload)
     else:
-        if not before_check(ObjectId(topic[0])):
+        if not before_check(topic[0]):
             return
         res = mongo.db.datas.insert_one({
             "device": DBRef("devices", topic[0]),
